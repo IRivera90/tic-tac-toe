defdd2ede8d26171ad1b68f6a0a28002e68a5b2e
IRivera90 <isacsac90@gmail.com>
2020-01-06 14:53:29 -0500

    work on authentification
    authentification working as intended

---
+++ assets/scripts/ui.js
@@ -0,0 +1,45 @@
+'use strict'
+const store = require('./store')
+const signUpSuccess = function (response) {
+  $('#message').text('Successfully signed up')
+  console.log(response)
+}
+const signUpFailure = function (error) {
+  $('#message').text('Sign up failed!')
+  console.log(error)
+}
+const signInSuccess = function (response) {
+  $('#message').text('Successfully signed in')
+  console.log(response)
+  store.user = response.user
+  $('#sign-up').hide()
+  $('#sign-in').hide()
+  $('#change-passwords').show()
+  $('#sign-out').show()
+}
+const signInFailure = function (error) {
+  $('#message').text('Sign in failed!')
+  console.log(error)
+}
+const changePasswordSuccess = function (response) {
+  $('#message').text('Password succesfully changed!')
+}
+const changePasswordFailure = function () {
+  $('#message').text('Change password failed')
+}
+const signOutSuccess = function () {
+  $('#message').text('Signed out')
+  $('#sign-up').show()
+  $('#sign-in').show()
+  $('#change-passwords').hide()
+  $('#sign-out').hide()
+}
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess
+}
